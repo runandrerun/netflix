61645940cce546a5e0629798480d717e26378a86
Andre <the.asantiagojr@gmail.com>
2020-11-17 17:05:24 -0500

    Barebones setup template.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Homepage from './pages/homepage/Homepage.component';
+
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
+};
